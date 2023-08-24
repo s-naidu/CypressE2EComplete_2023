4fa8f9ace3341a1f71ee00a07147ca10c325da50
sudarshan <dsnaidu07@gmail.com>
2023-08-24 13:26:16 +0530

    created dockerfile and docker compose files

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,29 @@
 const { defineConfig } = require("cypress");
 reporter: 'cypress-mochawesome-reporter',
 
-module.exports = defineConfig({
-  projectId: "yaewra",
-  reporter: 'mochawesome',
-  reporterOptions: {
-    useInlineDiffs: true,
-    embeddedScreenshots: true,
-    reportDir: 'cypress/results',
-    reportFilename: '[name].html',
-    overwrite: true,
-    html: true,
-    json: true,
-  },
-  watchForFileChanges:false,
-  chromeWebSecurity:false,
-  reporter: 'cypress-mochawesome-reporter',
-
-  e2e: {
-    //baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
-      return
+  module.exports = defineConfig({
+    projectId: "yaewra",
+    "reporter": "cypress-multi-reporters",
+    "reporterOptions": {
+      "reporterEnabled": "mochawesome",
+      "mochawesomeReporterOptions": {
+        "reportDir": "cypress/reports/mocha",
+        "quite": true,
+        "overwrite": false,
+        "html": false,
+        "json": true
+      }
     },
-  },
-});
+    watchForFileChanges: false,
+    chromeWebSecurity: false,
+    reporter: 'cypress-mochawesome-reporter',
+
+    e2e: {
+      //baseUrl: 'http://localhost:3000',
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+        require('cypress-mochawesome-reporter/plugin')(on);
+        return
+      },
+    },
+  });
